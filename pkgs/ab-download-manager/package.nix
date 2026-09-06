b3aa81ca555263e226b31415fbe5fbc365baea84
vamsi <n.vamsi9955@gmail.com>
2026-09-07 00:39:56 +0530

    5.01

--- pkgs/ab-download-manager/package.nix
+++ pkgs/ab-download-manager/package.nix
@@ -1,30 +1,69 @@
-{ lib, stdenv, fetchurl, autoPatchelfHook, makeWrapper, xorg, libglvnd, ... }:
+{ lib
+, stdenv
+, fetchurl
+, autoPatchelfHook
+, makeWrapper
+, libx11
+, libxrender
+, libxext
+, libxtst
+, libxi
+, libxcursor
+, libglvnd
+, fontconfig
+, freetype
+, alsa-lib
+, zlib
+, wayland
+, libxkbcommon
+}:
 
 stdenv.mkDerivation rec {
   pname = "ab-download-manager";
   version = "1.10.2";
 
   src = fetchurl {
-    url = "https://github.com/amir1376/ab-download-manager/releases/download/v1.10.2/ABDownloadManager_1.10.2_linux_arm64.tar.gz";
-    hash = "sha256-VMVISHe0e2LNiqsNZx+zf4mCC4xM5pY9BvBnK/2QGjE=";
+    url = "https://github.com/amir1376/ab-download-manager/releases/download/v${version}/ABDownloadManager_${version}_linux_x64.tar.gz";
+    hash = "sha256-xhwDnsQm3wC188/P0Htk7GKgw1x4vsvFYtRgDVpeqcQ=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
 
   buildInputs = [
-    xorg.libX11 xorg.libXrender xorg.libXext xorg.libXtst
-    xorg.libXi xorg.libXcursor libglvnd
+    # X11 Fallback Libraries
+    libx11 libxrender libxext libxtst libxi libxcursor 
+    
+    # Native Wayland & Graphics Libraries
+    wayland libxkbcommon libglvnd 
+    
+    # Fonts, Audio, and Core C++ Utils
+    fontconfig freetype alsa-lib zlib stdenv.cc.cc.lib
   ];
 
   installPhase = ''
     runHook preInstall
     
-    mkdir -p $out/opt/ab-download-manager $out/bin
-    cp -r * $out/opt/ab-download-manager/
+    mkdir -p $out/opt/ab-download-manager $out/bin $out/share/applications $out/share/pixmaps
+    cp -r bin lib $out/opt/ab-download-manager/
+    cp $out/opt/ab-download-manager/lib/ABDownloadManager.png $out/share/pixmaps/ab-download-manager.png
 
-    # Wrap the executable so it can find your system's graphics libraries
-    makeWrapper $out/opt/ab-download-manager/ABDownloadManager $out/bin/ab-download-manager \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}"
+    # Wrap the executable so it can dynamically locate the Wayland/X11 libraries on your system
+    makeWrapper $out/opt/ab-download-manager/bin/ABDownloadManager $out/bin/ab-download-manager \
+      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
+      --set SKIKO_RENDER_API "SOFTWARE"
+
+    cat <<EOF > $out/share/applications/ab-download-manager.desktop
+    [Desktop Entry]
+    Name=AB Download Manager
+    Comment=A Download Manager that speeds up your downloads
+    GenericName=Download Manager
+    Exec=ab-download-manager
+    Icon=ab-download-manager
+    Terminal=false
+    Type=Application
+    Categories=Network;Utility;
+    StartupWMClass=com-abdownloadmanager-desktop-AppKt
+    EOF
 
     runHook postInstall
   '';
@@ -34,5 +73,6 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/amir1376/ab-download-manager";
     license = licenses.gpl3Only;
     mainProgram = "ab-download-manager";
+    platforms = [ "x86_64-linux" ];
   };
 }
--- pkgs/omni-tools/package.nix
+++ pkgs/omni-tools/package.nix
@@ -1,5 +1,5 @@
 # pkgs/omni-tools/package.nix
-{ lib, buildNpmPackage, fetchFromGitHub }:
+{ lib, buildNpmPackage, fetchFromGitHub, git }:
 
 buildNpmPackage rec {
   pname = "omni-tools";
@@ -13,7 +13,8 @@ buildNpmPackage rec {
   };
 
   npmDepsHash = "sha256-DoWNUDmpaJAUVHRn3GnEI63QaEb8Te2WwYwJerFN+Ak=";
-
+  
+  nativeBuildInputs = [ git ];
   # By default, buildNpmPackage automatically runs 'npm run build' for us.
   
   installPhase = ''
